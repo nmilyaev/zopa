bb0fb30adb762ffb697118636e0ded665fb1e2ec
nmilyaev <n.milyaev@yahoo.co.uk>
2019-09-06 12:29:33 +0100

    Re- import project and fix gradle wrapper

--- src/main/java/com/zopa/service/RepaymentCalculator.java
+++ src/main/java/com/zopa/service/RepaymentCalculator.java
@@ -5,11 +5,20 @@ import java.math.RoundingMode;
 
 public class RepaymentCalculator {
 
-    public BigDecimal calculateMonthlyRepayment(int months, int amount, double annualRate){
-        double monthlyRate = annualRate/12;
-        double result = amount * (monthlyRate + (monthlyRate/(Math.pow(1+monthlyRate, months)-1)));
-        BigDecimal bd = BigDecimal.valueOf(result);
-        bd = bd.setScale(2, RoundingMode.DOWN);
-        return bd;
+    public BigDecimal calculateMonthlyRepayment(int months, int amount, double annualRate) {
+        BigDecimal monthlyRate = convert(annualRate).divide(convert(12));
+        BigDecimal total = convert(amount);
+        BigDecimal result = total.multiply(monthlyRate.add(monthlyRate.divide(
+                convert(Math.pow(1 + monthlyRate.intValue(), months) - 1)
+        )));
+        return result;
+    }
+
+    private BigDecimal convert(int value) {
+        return BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal convert(double value) {
+        return BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP);
     }
 }
